c2eda876b15562eaca744aa2bdca99584f0092c9
lpantojaa <lmpantoja@hotmail.com>
2023-07-24 20:16:47 +1000

    Get arp table from system.

--- Flask/login.py
+++ Flask/login.py
@@ -1,7 +1,10 @@
 # Import necessary libraries
-from flask import Flask
+from flask import Flask, render_template, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
-
+import os
+import re
+import requests
+from datetime import datetime
 
 # Initialize Flask application
 app = Flask(__name__)
@@ -15,6 +18,21 @@ db = SQLAlchemy(app)
 class MacAddress(db.Model):
     __tablename__ = 'mac_addresses'
     mac = db.Column(db.String, primary_key=True)
+
+# Function to get ARP table and parse it
+def get_arp_table():
+    arp_table = os.popen('arp -n').read()  # Get ARP table as string
+    ip_to_mac = {}
+
+    # Parse ARP table, line by line
+    for line in arp_table.split('\n')[1:]:
+        parts = re.split(r'\s+', line)
+        if len(parts) >= 3:
+            ip = parts[0]
+            mac = parts[2]
+            ip_to_mac[ip] = mac
+
+    return ip_to_mac
 
 # Run the application
 if __name__ == "__main__":
